a6dc76aacd1329e11e13cce840d60102e449cc1b
Bwn <basumatarybiswanath001@gmail.com>
2023-11-30 12:54:28 +0530

    code added

---
+++ script.js
@@ -0,0 +1,9 @@
+function menu_bar() {
+    let me_nu = document.getElementById("contain-er");
+    me_nu.style.left = "0px";
+}
+
+function closed() {
+    let cl_ose = document.getElementById("contain-er");
+    cl_ose.style.left = "-400px";
+}
